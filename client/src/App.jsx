ae4cfaf8b5bc9f3bd359098d7611dd9803bd3ad7
AnsariAhmedFrontEnd <am27043@gmail.com>
2024-09-10 18:30:10 +0530

    project basic setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+
+
+const App = () => {
+  return (
+    <div className="p-4 bg-gray-100 min-h-screen">
+      <button className="bg-blue-500 text-white hover:bg-blue-600">
+        Click Me
+      </button>
+    </div>
+  );
+};
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,33 @@
+//rLq2Nh4mE2k1FwjP
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+mongoose
+  .connect("mongodb+srv://am27043:rLq2Nh4mE2k1FwjP@cluster0.kc0s8.mongodb.net/")
+  .then(() => console.log("connected to mongoDB"))
+  .catch((error) => console.log(error));
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "DELETE", "PUT"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Cache-Control",
+    "Expires",
+    "Pragma",
+  ],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => {console.log(`server is now running on port ${PORT}`)});
